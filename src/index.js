ffd322aff97b7b17aadf55dc02fa92961869c9a9
Alex <epicYoshi64@gmail.com>
2018-11-05 07:03:44 -0800

    Adding Turns

--- src/index.js
+++ src/index.js
@@ -14,13 +14,18 @@ class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
 handleClick(i) {
     const squares = this.state.squares.slice();
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+    });
 }
 
     renderSquare(i) {
@@ -31,7 +36,7 @@ handleClick(i) {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
